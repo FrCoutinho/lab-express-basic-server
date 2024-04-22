27878249cb628196d7275ca254c7fbc5094b86b8
Francisco Coutinho <francisco.coutinho.so@gmail.com>
2024-04-22 22:29:33 +0100

    FT_PT_FranciscoCoutinho

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
+const express = require('express');
+const morgan = require('morgan');
 
 
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
 
+const app = express();
 
 
 // MIDDLEWARE
@@ -13,13 +16,21 @@
 // - `express.static()` to serve static files from the `public` folder
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
-
+app.use(express.json());
+app.use(express.static('public'));
+app.use(morgan("DEV"));
 
 
 // ROUTES
 // Start defining your routes here:
-
+app.get('/blog', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
+  });
 
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+const PORT = 5005;
+app.listen(PORT, () => {
+  console.log(` Express server listen on port ${PORT}`);
+});
